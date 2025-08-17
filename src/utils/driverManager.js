24a83af1210ee4a31ee882e67bb4db5887c233e8
Josecfr11 <josecfr11@gmail.com>
2025-08-16 20:31:12 -0700

    feat(testCasesOwners): Agrega dos casos de prueba para la opcion de owners

--- src/utils/driverManager.js
+++ src/utils/driverManager.js
@@ -1,15 +1,11 @@
 const { Builder } = require('selenium-webdriver');
 
 async function getDriver() {
-    // El 'selenium-manager' se encargará de encontrar y descargar el driver de Edge
-    console.log("Iniciando el WebDriver de Edge (gestión automática)...");
     const driver = await new Builder().forBrowser('MicrosoftEdge').build();
     return driver;
 }
-// Esta función cierra el WebDriver
 async function quitDriver(driver) {
     if (driver) {
-        console.log("Cerrando el navegador...");
         await driver.quit();
     }
 }
